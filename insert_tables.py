63b0c7b591d5335f0fae8ace22639efe1cd692c1
Michail27 <michail27.03@mail.ru>
2021-12-13 09:24:23 +0300

    change class InsertTables add IGNORE

--- insert_tables.py
+++ insert_tables.py
@@ -3,10 +3,10 @@ class InsertTables:
 
     @staticmethod
     def insert_rooms():
-        return 'INSERT INTO Rooms(id, name) ' \
+        return 'INSERT IGNORE INTO  Rooms(id, name) ' \
                'VALUES(%s, %s)'
 
     @staticmethod
     def insert_students():
-        return 'INSERT INTO Students(id, name, birthday, room, sex)' \
+        return 'INSERT IGNORE INTO Students(id, name, birthday, room, sex)' \
                ' VALUES(%s, %s, %s, %s, %s)'
