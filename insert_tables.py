6946f0bfe0f279ab42d7d99555f1357c6c63903e
Michail27 <michail27.03@mail.ru>
2021-12-17 16:20:24 +0300

    refactoring comments

--- insert_tables.py
+++ insert_tables.py
@@ -3,10 +3,9 @@ class InsertTables:
 
     @staticmethod
     def insert_rooms():
-        return 'INSERT IGNORE INTO  Rooms(id, name) ' \
-               'VALUES(%s, %s)'
+        return '''INSERT IGNORE INTO  Rooms(id, name) VALUES(%s, %s)'''
 
     @staticmethod
     def insert_students():
-        return 'INSERT IGNORE INTO Students(id, name, birthday, room, sex)' \
-               ' VALUES(%s, %s, %s, %s, %s)'
+        return '''INSERT IGNORE INTO Students(id, name, birthday, room, sex)
+         VALUES(%s, %s, %s, %s, %s)'''
